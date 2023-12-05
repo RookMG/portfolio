02d24c91ee348bde8acc6bec9134650bbcfcd35a
tngks1995 <tngks1995@naver.com>
2023-12-05 13:25:35 +0900

    Fix : 수상내역 잘못된 날짜 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -139,30 +139,35 @@ export const skillText = [
 ]
 
 export const awardText = [
-    {
-        title: "특화프로젝트 우수상 / 삼성청년SW아카데미",
-        desc: "삼성전자 주관 삼성청년SW아카데미 2학기 특화 프로젝트 우수상 (서울5반 2등) 수상",
-        date: "2023-10",
-    },
+  {
+    title: "자율프로젝트 우수상 / 삼성청년SW아카데미",
+    desc: "삼성전자 주관 삼성청년SW아카데미 2학기 자율 프로젝트 우수상 (기업연계반 3등) 수상",
+    date: "23.11.17",
+  },
+  {
+    title: "특화프로젝트 우수상 / 삼성청년SW아카데미",
+    desc: "삼성전자 주관 삼성청년SW아카데미 2학기 특화 프로젝트 우수상 (서울5반 2등) 수상",
+    date: "23.10.06",
+  },
     {
         title: "1등(구름상) / 구름톤 챌린지",
         desc: "구름(goorm) 주관 '구름톤 오프라인 팀 챌린지'에서 1등 (구름상) 수상",
-        date: "2023-09",
+        date: "23.09.23",
     },
     {
         title: "공통프로젝트 우수상 / 삼성청년SW아카데미",
         desc: "삼성전자 주관 삼성청년SW아카데미 2학기 공통 프로젝트 우수상 (서울1반 2등) 수상",
-        date: "2023-08",
+        date: "23.08.18",
     },
     {
         title: "우수상 / 2023 현대모비스 알고리즘 경진대회",
         desc: "현대모비스 주관 '2023 현대모비스 알고리즘 경진대회'(1억6000만원 규모)에서 우수상 수상",
-        date: "2023-07",
+        date: "23.07.07",
     },
     {
         title: "1학기 성적우수상 / 삼성청년SW아카데미",
         desc: "삼성전자 주관 삼성청년SW아카데미 1학기 코딩 집중과정 종합성적 우수상 (서울9반 2등) 수상",
-        date: "2023-05",
+        date: "23.05.26",
     },
 ]
 
@@ -170,37 +175,37 @@ export const certificationText = [
     {
         title: "SQLD (SQL 개발자) 자격증",
         desc: "한국데이터산업진흥원 SQL 개발자 국가공인 자격증 취득",
-        date: "2023-09",
+        date: "23.10.06",
     },
     {
         title: "OPIc(영어) IM2등급",
         desc: "ACTFL OPIc 영어 IM2등급 취득",
-        date: "2022-01",
+        date: "23.09.14",
     },
     {
         title: "HSAT lv.3",
         desc: "현대엔지비 주관 HSAT (현대자동차 코딩 인증평가) lv.3 취득",
-        date: "2023-07",
+        date: "23.08.11",
     },
     {
         title: "PCCP Java lv.5",
         desc: "그렙 주관 PCCP (프로그래머스 코딩전문역량인증) JAVA lv.5 취득",
-        date: "2023-07",
+        date: "23.07.16",
     },
     {
         title: "삼성 SW 역량테스트 B형",
         desc: "삼성전자 주관 SW 역량테스트 B형 (Pro) 취득",
-        date: "2023-04",
+        date: "23.04.29",
     },
     {
         title: "PCCP Python lv.4",
         desc: "그렙 주관 PCCP (프로그래머스 코딩전문역량인증) Python lv.4 취득",
-        date: "2022-09",
+        date: "22.09.18",
     },
     {
         title: "TOEIC(영어) 900점",
         desc: "한국토익위원회 TOEIC 900점 (990점 만점)",
-        date: "2022-01",
+        date: "22.01.08",
     },
 ]
 
@@ -208,8 +213,8 @@ export const projectText = [
     {
         text: ["잉크젯 디스플레이 프린팅에 사용되는", "천만 개 단위의 픽셀을 한번에 편집", "CAD 그래픽 편집 툴"],
         title: "SEMES Pixel Designer",
-        code: "",
-        view: "",
+        code: "https://github.com/RookMG/spd",
+        view: "https://github.com/RookMG/spd",
         info: [
             "window application",
             "period : 6 weeks",
@@ -220,8 +225,8 @@ export const projectText = [
     {
         text: ["냉장고 식재료 관리", "재료 기반 레시피 추천", "남는 식재료 위치 기반 나눔"],
         title: "신선나눔냉장고",
-        code: "",
-        view: "",
+        code: "https://github.com/RookMG/snn",
+        view: "https://github.com/RookMG/snn",
         info: [
             "android application",
             "period : 6 weeks",
@@ -233,7 +238,7 @@ export const projectText = [
         text: ["멀리 있어도", "같은 공간에 있는 것 처럼,", "모여서 찍는 셀카"],
         title: "MOZZI",
         code: "https://github.com/TeamGarlic/Mozzi",
-        view: "",
+        view: "https://github.com/TeamGarlic/Mozzi",
         info: [
             "web site",
             "period : 6 weeks",
@@ -245,7 +250,7 @@ export const projectText = [
         text: ["관광지 추천 커뮤니티", "나만의 여행 코스 편집 & 공유", "유명 관광지 이상형 월드컵"],
         title: "여행어때",
         code: "https://github.com/RookMG/EnjoyTrip",
-        view: "",
+        view: "https://github.com/RookMG/EnjoyTrip",
         info: [
             "web site",
             "period : 1 week",
@@ -288,6 +293,6 @@ export const footerText = [
     {
         title: "PS Records & Code",
         desc: "백준, SWEA, 프로그래머스의 문제 풀이 코드를 확인할 수 있습니다",
-        link: "https://solved.ac/profile/tngks1995",
+        link: "https://github.com/RookMG/BaekJoon",
     },
 ];
